dcfd7c4ad762cef164640231a73f2fb062eea167
Tanyusha7 <swanonlake@gmail.com>
2023-07-16 23:41:39 +0200

    add components

---
+++ src/components/pages/Movies.jsx
@@ -0,0 +1,12 @@
+// import Header from 'components/Header/Header';
+import React from 'react';
+
+const Movies = () => {
+  return (
+    <>
+      <div> Movies</div>
+    </>
+  );
+};
+
+export default Movies;
