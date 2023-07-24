b83b0db6eb305567b68017be05450c1c96eaaa2b
Tanyusha7 <120665644+Tanyusha7@users.noreply.github.com>
2023-07-24 07:59:22 +0000

    Deploying to gh-pages from @ Tanyusha7/goit-react-hw-05-movies@c820d9a8f41da3ec5ee8cdea68b77b1102b283e5 🚀

--- static/js/180.8280e3ab.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[180],{5259:function(n,e,t){t.d(e,{x:function(){return c}});var i,r=t(168),c=t(5706).Z.p(i||(i=(0,r.Z)(["\n  margin-top: 20px;\n  padding-left: 10px;\n  color: red;\n  font-size: 18px;\n  line-height: 1.33;\n"])))},6180:function(n,e,t){t.r(e);var i=t(5259),r=t(184);e.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(i.x,{children:"We didn't find such page!!!"})})}}}]);
-//# sourceMappingURL=180.8280e3ab.chunk.js.map
